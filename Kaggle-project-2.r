4ce64b64a8cf7d5e8caa9c450509a33d9d2192aa
git874997967 <g874997967@gmail.com>
2018-02-13 14:10:45 -0800

    根据第一个  项目的经验完成 鬼怪的分类预测   完成没有问题 但是精度有待提高 继续

--- Kaggle-project-2.r
+++ Kaggle-project-2.r
@@ -1,6 +1,12 @@
 library(kknn)
 library(class)
 library(sqldf)
+library(ranger)
+library(randomForest)
+library(InformationValue) 
+library(dplyr)
+library(party)
+library(caret)
 gost_train=read.csv(file.choose())
 gost_test=read.csv(file.choose())
 str(gost_train)
@@ -45,7 +51,7 @@ legend(
   levels(gost_train$type),
   fill=col
 )
-
+box()
 ####color analysis
 gost_train$
 Ghost_bone_length=sqldf("select bone_length, type from gost_train where type='Ghost' ")
@@ -62,6 +68,7 @@ legend(
   levels(gost_train$type),
   fill=col
 )
+box()
 ### rotting length
 gost_train$rotting_flesh
 Ghost_rotting_flesh=sqldf("select rotting_flesh, type from gost_train where type='Ghost' ")
@@ -78,6 +85,7 @@ legend(
   levels(gost_train$type),
   fill=col
 )
+box()
 ###hair_length
 gost_train$hair_length
 Ghost_hair_length=sqldf("select hair_length, type from gost_train where type='Ghost' ")
@@ -94,4 +102,84 @@ legend(
   levels(gost_train$type),
   fill=col
 )
+box()
+### data intergration
 
+data=bind_rows(gost_train,gost_test)
+train.row=1:nrow(gost_train)
+test.row=1:nrow(gost_test)
+
+
+## Use woe and Iv failed
+#used in binary only!!!
+#WOE(X=factor(data$bone_length[1:nrow(gost_train)]),Y=data$type[1:nrow(gost_train)])
+#IV(X=factor(data$bone_length[1:nrow(gost_train)]),Y=data$type[1:nrow(gost_train)])
+
+###  test 1
+Ghost_feature2=Ghost_hair_length$hair_length*Ghost_bone_length$bone_length*Ghost_hair_length$hair_length*Ghost_soul$has_soul
+Ghoul_feature2=Ghoul_hair_length$hair_length*Ghoul_bone_length$bone_length*Ghoul_hair_length$hair_length* Ghoul_soul$has_soul
+Goblin_feature2=Goblin_hair_length$hair_length*Goblin_bone_length$bone_length*Goblin_hair_length$hair_length*Goblin_soul$has_soul
+ 
+Ghost_feature=Ghost_hair_length$hair_length*Ghost_bone_length$bone_length*Ghost_hair_length$hair_length*Ghost_rotting_flesh$rotting_flesh*Ghost_soul$has_soul
+Ghoul_feature=Ghoul_hair_length$hair_length*Ghoul_bone_length$bone_length*Ghoul_hair_length$hair_length*Ghoul_rotting_flesh$rotting_flesh*Ghoul_soul$has_soul
+Goblin_feature=Goblin_hair_length$hair_length*Goblin_bone_length$bone_length*Goblin_hair_length$hair_length*Goblin_rotting_flesh$rotting_flesh*Goblin_soul$has_soul
+
+plot(density(
+  Ghost_feature2 
+  ),ylim=c(0,100),xlim=c(-0.1,0.3),main="data features2  and type" ,xlab="has_soul value")
+polygon(density(
+  Ghost_feature2 
+  ), col=rgb(1,1,0,0.2), border="blue", lwd=2) 
+polygon(density(
+  Ghoul_feature2 
+  ), col=rgb(1,0,1,0.2), border="red", lwd=2) 
+polygon(density(
+  Goblin_feature2 
+  ), col=rgb(0,1,1,0.2), border="red", lwd=2) 
+legend(
+  "topleft",
+  levels(gost_train$type),
+  fill=col
+)
+box()
+###build the model with cforest
+str(data)
+set.seed(415)
+#ok 33 33 34
+#model_cforest <- cforest(type ~bone_length + rotting_flesh + hair_length + has_soul + color+bone_length*rotting_flesh*hair_length*has_soul, data = data[train.row, ], controls=cforest_unbiased(ntree=2000, mtry=3))
+#ok 32 34 39 better!!!
+#model_rpart <- rpart(type ~ color+bone_length + rotting_flesh + hair_length + has_soul , data = data[train.row, ],cp=0.0015,method='class')
+# 35 34 42!!!!! Perfect?!
+ model_ranger <- ranger(type ~ bone_length + rotting_flesh + hair_length + has_soul + color, data = data[train.row, ], num.trees=3000,num.threads=8)
+ 
+ # model_kknn<- cforest(type ~ bone_length + rotting_flesh + hair_length + has_soul + color+bone_length*rotting_flesh*hair_length*has_soul, data = data[train.row, ])
+# cv.summarize <- function(data.true, data.predict) {
+#   print(paste('Recdata:', Recdata(data.true, data.predict)))
+#   print(paste('Precision:', Precision(data.true, data.predict)))
+#   print(paste('Accuracy:', Accuracy(data.predict, data.true)))
+#   print(paste('AUC:', AUC(data.predict, data.true)))
+# }
+  
+set.seed(415)
+ cv.test.sample <- sample(1:nrow(gost_train), as.integer(0.3 * nrow(gost_train)), replace = TRUE)
+ cv.test <- data[cv.test.sample,]
+#cv.predictioncf <- predict(model_cforest, cv.test, OOB=TRUE, type = "response")
+#cv.predictionrp <- predict(model_rpart, cv.test, OOB=TRUE, type = "class")
+cv.predictionra <- predict(model_ranger,cv.test, interval = "prediction",  type = "response")
+
+#cv.summarize(cv.test$type, cv.predictioncf)
+table(cv.test$type,cv.predictionra$predictions)
+sum(diag(prop.table( table(train$type,model_ranger$predictions) )))
+ #print(model_rpart,digit=2)
+ #plotcp(model_rpart,lty=1,col=2)
+ 
+#finalcf=predict(model_cforest,data[(1+nrow(gost_train)):(nrow(data)),],OOB=T,type='response')
+ finalranger=predict(model_ranger,data[(1+nrow(gost_train)):(nrow(data)),],OOB=T,type='response')
+finalrpat=predict(model_rpart,data[(1+nrow(gost_train)):(nrow(data)),],OOB=T,type='class')
+ ranger_result <- data.frame(id = gost_test$id, type = final$predictions)
+ # rpart_result=data.frame(id=gost_test$id,type=finalrpat)
+ # cf_result=data.frame(id=gost_test$id,type=finalcf)
+  write.csv(cf_result, file = "ranger3.csv", row.names = FALSE)
+ # table(gost_test,final$predictions)
+ # finalcf
+ 
